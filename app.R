322b3dc2ff53332b6cbd5247a578722bf465c20a
Alexandre Freitas <alexandrefreitas92@gmail.com>
2022-05-24 16:15:17 -0300

    atualizando

--- app.R
+++ app.R
@@ -1,14 +1,54 @@
-library(shiny)
-
-ui <- fluidPage(
-  selectInput("var", "Variable", names(mtcars)),
-  numericInput("bins", "bins", 10, min = 1),
-  plotOutput("hist")
+# Define the UI
+ui = tagList(
+  navbarPage(
+    # theme = "cerulean",  # <--- To use a theme, uncomment this
+    "shinythemes",
+    tabPanel("Página Inicial", "Aqui vai ter as imagens para escolher para qual painel a pessoa quer ir"),
+    tabPanel("Programas",
+             sidebarPanel(
+               fileInput("file", "File input:"),
+               textInput("txt", "Text input:", "general"),
+               sliderInput("slider", "Slider input:", 1, 100, 30),
+               tags$h5("Default actionButton:"),
+               actionButton("action", "Search"),
+               
+               tags$h5("actionButton with CSS class:"),
+               actionButton("action2", "Action button", class = "btn-primary")
+             ),
+             mainPanel(
+               tabsetPanel(
+                 tabPanel("Tab 1",
+                          h4("Table"),
+                          tableOutput("table"),
+                          h4("Verbatim text output"),
+                          verbatimTextOutput("txtout"),
+                          h1("Header 1"),
+                          h2("Header 2"),
+                          h3("Header 3"),
+                          h4("Header 4"),
+                          h5("Header 5")
+                 ),
+                 tabPanel("Tab 2", "This panel is intentionally left blank"),
+                 tabPanel("Tab 3", "This panel is intentionally left blank")
+               )
+             )
+    ),
+    tabPanel("ODS", "Tabela com os ODS"),
+    tabPanel("Sobre", "Informações sobre o Sistema de Monitoramento e link para o Github")
+  )
 )
-server <- function(input, output, session) {
-  data <- reactive(mtcars[[input$var]])
-  output$hist <- renderPlot({
-    hist(data(), breaks = input$bins, main = input$var)
-  }, res = 96)
+
+
+# Define the server code
+server = function(input, output) {
+  output$txtout <- renderText({
+    paste(input$txt, input$slider, format(input$date), sep = ", ")
+  })
+  output$table <- renderTable({
+    head(cars, 4)
+  })
 }
-shinyApp(ui, server)
+
+
+# Return a Shiny app object
+shinyApp(ui = ui, server = server)
