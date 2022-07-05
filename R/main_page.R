6600874ece933bc2a4d269932c8abd3c790dbc26
Alexandre Freitas <alexandrefreitas92@gmail.com>
2022-07-05 11:54:40 -0300

    incluir bandeira mg

--- R/main_page.R
+++ R/main_page.R
@@ -2,7 +2,7 @@ main_page <- function() {
   "Aqui vai ter as imagens para escolher para qual painel a pessoa quer ir."
   fluidRow(
     column(4, offset = 1,
-           tags$a(tags$img(src = "images/logo_gov_mg.png", onclick="customHref('programs_panel')",
+           tags$a(tags$img(src = "images/bandeira-do-estado-de-minas-gerais.png", onclick="customHref('programs_panel')",
                            height="80%", width="80%")),
            actionButton("ir_tab_programs", 'Vá para a aba dos Programas')
            
